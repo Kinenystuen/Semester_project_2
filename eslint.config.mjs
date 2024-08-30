7962267a43721728e756b3775ccf3a2d9f798129
Kinenystuen <kine_on@hotmail.com>
2024-08-30 12:59:24 +0200

    updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+    },
+  },
   pluginJs.configs.recommended,
 ];
