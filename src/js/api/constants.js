58cf0f4f752b5d36d43987d05c29467c6bfe828c
Kinenystuen <kine_on@hotmail.com>
2024-09-03 11:15:38 +0200

    constants for fetching api

---
+++ src/js/api/constants.js
@@ -0,0 +1,5 @@
+export const apiHostUrl = 'https://v2.api.noroff.dev';
+export const apiAuth = '/auth';
+export const apiLogin = '/login';
+export const apiRegister = '/register';
+export const apiAction = '/auction/listings';
