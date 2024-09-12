19fb83ec6f6672622256c0f5f4651ddc7c8d6478
Kinenystuen <kine_on@hotmail.com>
2024-09-12 13:23:04 +0200

    Export functions

--- src/js/api/handlers/index.js
+++ src/js/api/handlers/index.js
@@ -1,3 +1,7 @@
 export { setLoginForm } from './login.js';
 export { setLogOut } from './logout.js';
 export { setRegisterForm } from './register.js';
+export { setUpdateProfileForm } from './updProfile.js';
+export { setBiddingForm } from './bid.js';
+export { setUpdateListingForm } from './updListing.js';
+export * from './createListing.js';
