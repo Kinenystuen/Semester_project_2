cc9ddea3826dfe278c01551d3c1a25a0b4e870d5
Kinenystuen <kine_on@hotmail.com>
2024-09-12 15:14:58 +0200

    Added secrets

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,5 +1,9 @@
-export const apiHostUrl = 'https://v2.api.noroff.dev';
-export const apiAuth = '/auth';
-export const apiLogin = '/login';
-export const apiRegister = '/register';
-export const apiAction = '/auction/listings';
+export const apiHostUrl = import.meta.env.VITE_API_HOST_URL;
+export const apiAuth = import.meta.env.VITE_API_AUTH;
+export const apiLogin = import.meta.env.VITE_API_LOGIN;
+export const apiRegister = import.meta.env.VITE_API_REGISTER;
+export const apiAction = import.meta.env.VITE_API_ACTION;
+export const apiProfiles = import.meta.env.VITE_API_PROFILES;
+export const apiListings = import.meta.env.VITE_API_LISTINGS;
+
+export const apiKey = import.meta.env.VITE_API_KEY;
