8920cd846e0ace30147d1760920160855c6418fe
Kinenystuen <kine_on@hotmail.com>
2024-09-12 14:19:31 +0200

    Export functions

--- src/js/evtListeners/index.js
+++ src/js/evtListeners/index.js
@@ -3,3 +3,5 @@ export * from './formValidation.js';
 export * from './search.js';
 export * from './getTag.js';
 export * from './registerBtn.js';
+export * from './updImgLive.js';
+export * from './sortListings.js';
