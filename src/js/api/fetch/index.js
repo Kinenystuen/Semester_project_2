e290f08867f2612f9937920038e601f1f489c8f7
Kinenystuen <kine_on@hotmail.com>
2024-09-12 13:21:52 +0200

    Export functions

---
+++ src/js/api/fetch/index.js
@@ -0,0 +1,7 @@
+export * from './apiTags.js';
+export * from './apiProfile.js';
+export * from './updListing.js';
+export * from './deleteListing.js';
+export * from './bid.js';
+export * from './createListing.js';
+export * from './updProfile.js';
