cc1eb4a6a93f0dee72ce4aa82e938ed797b21fab
Kinenystuen <kine_on@hotmail.com>
2024-09-16 16:59:03 +0200

    index js file with routes, calling functions depending on login status and window location

---
+++ src/js/index.js
@@ -0,0 +1,81 @@
+import * as evtListeners from './evtListeners/index.js';
+import * as fetch from './api/fetch/index.js';
+import * as handlers from './api/handlers/index.js';
+import * as status from './api/auth/index.js';
+import * as display from './display/index.js';
+import { getListsURL } from './api/listings/getListings.js';
+import { selUrl } from './api/listings/getListItem.js';
+import { clearHTML } from './utilitis/clearHTML.js';
+import { displayProfileListings } from './display/displayProfileListings.js';
+import { tooltips } from './utilitis/tooltips.js';
+
+const currentUrl = window.location.href;
+const path = window.location.pathname;
+
+// Check if the user is logged in
+document.addEventListener('DOMContentLoaded', function () {
+  const loggedIn = status.isLoggedIn();
+
+  // If the current page is not an /auth page, toggle login visibility
+  if (
+    !currentUrl.includes('/auth/login') &&
+    !currentUrl.includes('/auth/signup')
+  ) {
+    display.toggleLoginVisibility(loggedIn);
+  }
+  // If user is logged in, set logout functionality and update profile dropdown
+  if (loggedIn) {
+    handlers.setLogOut();
+    display.updateProfileDD();
+    const profileLink = document.getElementById('profileLink');
+    profileLink.classList.remove('d-none');
+
+    if (currentUrl.includes('pages/profile.html')) {
+      const profileLoggedOutContainer = document.getElementById(
+        'profileLoggedOutContainer',
+      );
+      clearHTML(profileLoggedOutContainer);
+      display.displayProfile(fetch.getProfileURL);
+      displayProfileListings(fetch.getProfileListingsUrl);
+    }
+    if (currentUrl.includes('pages/profiles.html')) {
+      display.displayProfiles(fetch.getProfilesURL);
+      evtListeners.searchProfiles();
+      evtListeners.setupPagination('profiles');
+    }
+  } else {
+    if (currentUrl.includes('pages/profile.html')) {
+      const profileContainerArea = document.getElementById(
+        'profileContainerArea',
+      );
+      clearHTML(profileContainerArea);
+    }
+  }
+  tooltips();
+});
+
+if (path === '/' || path === '' || path === '/index.html') {
+  display.displaySlider(getListsURL);
+  evtListeners.registerBtn();
+}
+if (currentUrl.includes('pages/auctions.html')) {
+  display.displayListings(getListsURL);
+  evtListeners.searchListings();
+  fetch.fetchAndPopulateTags(getListsURL);
+  evtListeners.sortListings();
+  evtListeners.setupPagination('listings');
+}
+if (currentUrl.includes('pages/listingitem.html')) {
+  display.displayListingItem(selUrl);
+}
+
+if (currentUrl.includes(`/auth/login`)) {
+  handlers.setLoginForm();
+  evtListeners.initFormValidation('loginForm');
+  evtListeners.historyBack('loginContainer', 'bodyOverlay');
+}
+if (currentUrl.includes(`/auth/signup`) || path === `/html/auth/signup.html`) {
+  handlers.setRegisterForm();
+  evtListeners.initFormValidation('registerForm');
+  evtListeners.historyBack('registerForm', 'bodyOverlay');
+}
