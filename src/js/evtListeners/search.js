28543b05304ff0037d44d61685233900da426544
Kinenystuen <kine_on@hotmail.com>
2024-09-16 11:48:23 +0200

    Added search profiles function

--- src/js/evtListeners/search.js
+++ src/js/evtListeners/search.js
@@ -1,8 +1,8 @@
-// listings.js (refactored)
-import { displayListings } from '../display/displayListings.js';
+import * as display from './../display/index.js';
 import * as constants from './../api/constants.js';
 import { getListsURL } from '../api/listings/getListings.js';
 import { clearHTML } from '../utilitis/clearHTML.js';
+import { getProfilesURL } from '../api/fetch/apiProfiles.js';
 
 export async function searchListings() {
   const activeListings = document.getElementById('listings');
@@ -16,12 +16,30 @@ export async function searchListings() {
       if (searchInputTrim.length > 0) {
         const newUrl = `${constants.apiHostUrl}${constants.apiAction}/search?q=${encodeURIComponent(searchInputTrim)}`;
         clearHTML(activeListings);
-        displayListings(newUrl);
+        display.displayListings(newUrl);
       } else {
-        displayListings(getListsURL);
+        display.displayListings(getListsURL);
       }
     });
   }
 }
-// Automatically initialize on window load
-window.onload = searchListings;
+export async function searchProfiles() {
+  const profilesDisplay = document.getElementById('profilesDisplay');
+
+  // Search Functionality
+  const searchInputP = document.querySelector('#search-input');
+
+  if (searchInputP) {
+    searchInputP.addEventListener('keyup', function () {
+      const searchInputTrim = searchInputP.value.trim();
+      if (searchInputTrim.length > 0) {
+        const newUrl = `${constants.apiHostUrl}${constants.apiProfiles}/search?q=${encodeURIComponent(searchInputTrim)}`;
+        console.log(newUrl);
+        clearHTML(profilesDisplay);
+        display.displayProfiles(newUrl);
+      } else {
+        display.displayProfiles(getProfilesURL);
+      }
+    });
+  }
+}
