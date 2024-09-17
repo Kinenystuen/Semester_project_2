6a903a4b292d15444001534cd724b01c917d1811
Kinenystuen <kine_on@hotmail.com>
2024-09-17 15:52:08 +0200

    rollupOptions to define the entry points for each html

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
-// vite.config.js
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/Semester_project_2/',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        auctions: resolve(__dirname, 'html/pages/auctions.html'),
+        profiles: resolve(__dirname, 'html/pages/profiles.html'),
+        profile: resolve(__dirname, 'html/pages/profile.html'),
+        listingitem: resolve(__dirname, 'html/pages/listingitem.html'),
+        login: resolve(__dirname, 'html/auth/login.html'),
+        signup: resolve(__dirname, 'html/auth/signup.html'),
+      },
+    },
   },
   server: {
     port: 8080,
-    historyApiFallback: true, // Ensure proper routing for SPAs
   },
   preview: {
     port: 8080,
