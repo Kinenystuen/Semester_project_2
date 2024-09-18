f17259e09c53d6b3fc61c97f3c8978408d314835
Kinenystuen <kine_on@hotmail.com>
2024-09-18 18:11:46 +0200

    When deleting listing, locate to profile

--- src/js/api/fetch/deleteListing.js
+++ src/js/api/fetch/deleteListing.js
@@ -5,7 +5,7 @@ import * as constants from './../constants.js';
 
 const method = 'delete';
 
-export async function deleteListing(listingData) {
+export async function deleteListing(listingData, listData) {
   const apiUrl = `${constants.apiHostUrl}${constants.apiAction}/${listingData.id}`;
   try {
     const btnCreateListing = document.getElementById('btnCreateListing');
@@ -25,7 +25,11 @@ export async function deleteListing(listingData) {
     const response = await fetch(`${apiUrl}`, options);
 
     if (response.ok) {
-      window.location.reload();
+      try {
+        window.location.href = `html/pages/profile.html?user=${listData.seller.name}`;
+      } catch {
+        window.location.href = `html/pages/auctions.html`;
+      }
     } else {
       if (btnCreateListing) {
         btnCreateListing.innerHTML = 'Delete listing';
