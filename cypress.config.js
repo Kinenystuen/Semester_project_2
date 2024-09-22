14cfe27fd11ecec5d6725a9bc88b6fade53bc8ce
Kinenystuen <kine_on@hotmail.com>
2024-09-22 23:41:24 +0200

    cypress

--- cypress.config.js
+++ cypress.config.js
@@ -7,13 +7,14 @@ dotenv.config();
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://kinenystuen.github.io/Semester_project_2/',
-    setupNodeEvents(on, config) {
-      // Implement node event listeners here
-    },
-    defaultCommandTimeout: 15000,
-    requestTimeout: 15000,
+    setupNodeEvents(on, config) {},
+    defaultCommandTimeout: 20000,
+    requestTimeout: 20000,
     responseTimeout: 20000,
-    pageLoadTimeout: 30000,
+    pageLoadTimeout: 60000,
+    retries: {
+      runMode: 2,
+    },
     env: {
       email: process.env.email,
       password: process.env.password,
