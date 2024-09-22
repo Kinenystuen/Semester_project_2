0a5630606dc5244cc3a395f54b4e69e22c63d873
Kinenystuen <kine_on@hotmail.com>
2024-09-22 22:59:01 +0200

    updated

--- cypress.config.js
+++ cypress.config.js
@@ -10,9 +10,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // Implement node event listeners here
     },
-    defaultCommandTimeout: 10000,
-    requestTimeout: 10000,
-    responseTimeout: 15000,
+    defaultCommandTimeout: 15000,
+    requestTimeout: 15000,
+    responseTimeout: 20000,
+    pageLoadTimeout: 30000,
     env: {
       email: process.env.email,
       password: process.env.password,
